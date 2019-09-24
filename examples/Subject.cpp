f699fbefb221e4f1d8c8a067f5051e583b117973
ctuu <ctuu1s@outlook.com>
2019-09-24 16:28:24 +0800

    update subject example

--- examples/Subject.cpp
+++ examples/Subject.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <any>
 #include "rx.h"
 
 int main()
 {
     auto subject = rx::subject<int>();
-    subject.next(3);
-    subject.subscribe([](int i) { std::cout << "Next: " << i << std::endl; });
+    // subject.next(3);
+    subject.subscribe(
+        [](int i) { std::cout << "Next: " << i << std::endl; }, //next
+        [](std::any err) { std::cout << "Error code: " << std::any_cast<int>(err) << std::endl; });
     subject.next(4);
     subject.next(5);
+    subject.error(404);
     return 0;
 }
